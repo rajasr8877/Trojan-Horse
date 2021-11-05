a66dd45ab66e3e8bab28659bf9d1aa9059d40e10
rajasr8877 <s.r.raja999888@gmail.com>
2021-11-05 18:51:28 +0530

    New Files Added Helloworld example

--- demo-1/src/main/java/com/raja/demo/Helloworld.java
+++ demo-1/src/main/java/com/raja/demo/Helloworld.java
@@ -1,5 +1,14 @@
 package com.raja.demo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class Helloworld {
+	
+	@RequestMapping("/hello")
+	public String hell()
+	{
+		return "Hello World Spring Project ";
+	}
 
 }
